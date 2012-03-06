dcd579764aa4c39c20038e3c8edec45ad688b7dc
Raul Jara <raul.c.jara@gmail.com>
2012-03-06 16:43:01 -0500

    Working on general tools.

---
+++ lib/CriteriaLists.hs
@@ -0,0 +1,26 @@
+module CriteriaLists
+( CritList(..)
+, grabHead
+, lst
+) where
+
+import Iterations
+import Data.List
+
+type CritList a = [[a]]
+
+cleanse :: CritList a -> CritList a
+cleanse = filter $ (>0) . length
+
+grabHead :: CritList a -> Int -> (a, CritList a)
+grabHead xs i = (item, remaining)
+  where
+    item = head $ xs !! i
+    remaining = cleanse $ mapWithIndex skipI xs
+      where
+        skipI (xs', n)
+          | n == i    = tail xs'
+          | otherwise = xs'
+
+lst :: CritList Int
+lst = map (\n-> [1..n]) [1..8]
